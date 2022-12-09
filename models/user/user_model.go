9c7b7390196427afd68b1be0d001b91c21bcd432
wxsatellite <1453085314@qq.com>
2022-12-09 14:14:51 +0800

    模型

---
+++ models/user/user_model.go
@@ -0,0 +1,7 @@
+package user
+
+import "sample/models"
+
+type User struct {
+	models.BaseModel
+}
---
+++ models/user/user_util.go
@@ -0,0 +1,10 @@
+package user
+
+import "sample/pkg/database"
+
+// IsEmailExist 判断 Email 已被注册
+func IsEmailExist(email string) bool {
+	var count int64
+	database.DB.Model(User{}).Where("email = ?", email).Count(&count)
+	return count > 0
+}
